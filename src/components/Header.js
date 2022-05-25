d6228c54ad076b916fc93f4d9cba73b35291bef5
Tiffany Ng <tiffany.ng@sg.ey.com>
2022-05-25 23:46:24 +0800

    part 2.1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Guess the word!</p>
+      <p>Enter a letter to start</p>
+    </>
+  )
+}
+
+export default Header
